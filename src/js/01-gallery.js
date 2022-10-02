1fdacc429921cfae4e666b0a05aa6162ba29fd09
Filatov Dmytro <dmitrovfilatov@gmail.com>
2022-10-02 20:40:35 +0300

    add simplelights

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,9 @@
+
+
 // Add imports above this line
+import SimpleLightbox from "simplelightbox"
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+console.log(SimpleLightbox);
 console.log(galleryItems);
